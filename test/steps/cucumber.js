f23d6482e4ece034ea9c65d55b5d92f46983128f
arreche <arreche@gmail.com>
2016-12-05 18:30:22 +0000

    setup yadda for cucumber like testing

---
+++ test/steps/cucumber.js
@@ -0,0 +1,15 @@
+var English = require('yadda').localisation.English;
+
+module.exports = English.library()
+  .given("I wrote a feature in test/features", () => {
+
+  })
+  .when("I wrote the step in test/steps", () => {
+
+  })
+  .when("I run npm test", () => {
+
+  })
+  .then("the result is displayed on the console", () => {
+
+  });
