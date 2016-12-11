c5ff8fe60118b5f75174a98dc9abc86b6b32ab11
Adrian Booth <adrian.booth@yourgolftravel.com>
2016-12-11 22:02:54 +0000

    post story to backend on new vote, but js is acting weird and wont let me pass the story id on after the post

--- src/controller.js
+++ src/controller.js
@@ -1,3 +1,5 @@
+var request = require("superagent")
+
 module.exports = function(bot, controller){
   var help = 'go to channel you want vote and direct mention me `start new vote "title" http://ticket.url` to start vote'
 
@@ -19,17 +21,38 @@ module.exports = function(bot, controller){
               };
   var instructions = 'Please DM me with: `vote 1` (Simple), `vote 2` (Medium) or `vote 3` (Hard) - Discussion in ticket or here as you prefer. :slightly_smiling_face:'
 
+  function setStoryId(res){
+    story = res.text
+    return story._id
+  }
+
   controller.hears('start new vote',['direct_message','direct_mention'],function(bot,message) {
+    var storyId
     votes = [];
     start_channel = message.channel;
     match = message.text.match(/start new vote\s+(.*)\s+<?(http.*)>/)
     story.name = match[1]
     story.url = match[2];
+    request.post('localhost:3000/stories/')
+    .set('Content-Type', 'application/json')
+    .send({name: story.name, url: story.url, size: 0})
+    .end(function(err, res){
+      console.log(res.text) // why can I log out res.text here but not res.text._id
+      console.log("POSTED A NEW STORY")
+    })
     bot.reply(message,'<!channel> NEW ASYNC VOTE on <' + story.url + '|' + story.name + '> ' + instructions);
   });
 
   controller.hears('vote',['direct_message'],function(bot,message) {
+    console.log("VOTING ON A STORY")
+    console.log(storyId)
     vote = message.text.match(/\d+/)[0]
+    // request.post('http://master.bass-seahorse-cod.app.push.drieapp.co/stories/' + storyId + "/votes")
+    // .set('Content-Type', 'application/json')
+    // .send({size: vote})
+    // .end(function(err, res){
+    //   console.log("POSTED A NEW VOTE")
+    // })
     votes.push({vote:vote, user:message.user});
     bot.reply(message,'I received your vote: ' + vote +  ' <@'+message.user+'>');
     bot.say({channel: start_channel, text: '<!here> ASYNC VOTE UPDATE '+ summaryText(votes)+ ' on <' + story.url + '|' + story.name + '> '});
