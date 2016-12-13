4feafd112043b34d68083aae5100bb32ebafdd9b
arreche <arreche@gmail.com>
2016-12-13 07:07:46 +0000

    remove dummy tests

--- test/features/cucumber.feature
+++
@@ -1,8 +0,0 @@
-Feature: Cucumber Tests
-
-Scenario: Should run cucumber like tests
-
-   Given I wrote a feature in test/features
-   And I wrote the step in test/steps
-   When I run npm test
-   Then the result is displayed on the console
--- test/yadda.js
+++ test/yadda.js
@@ -1,19 +1,18 @@
 var Yadda = require('yadda');
 Yadda.plugins.mocha.StepLevelPlugin.init();
 
-new Yadda.FeatureFileSearch('./test/features').each(function(file) {
+new Yadda.FeatureFileSearch('./test/features').each(function (file) {
 
-    featureFile(file, function(feature) {
+    featureFile(file, function (feature) {
 
-        var cucumber = require('./steps/cucumber');
         var hello = require('./steps/hello');
         var voting = require('./steps/voting');
 
-        var yadda = Yadda.createInstance([cucumber, hello, voting]);
+        var yadda = Yadda.createInstance([hello, voting]);
 
-        scenarios(feature.scenarios, function(scenario) {
+        scenarios(feature.scenarios, function (scenario) {
             var ctx = {};
-            steps(scenario.steps, function(step, done) {
+            steps(scenario.steps, function (step, done) {
                 yadda.run(step, { ctx: ctx }, done);
             });
         });
